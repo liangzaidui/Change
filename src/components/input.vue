a4c680ef495a67515a5ae71e5c66dfe3af654ebb
lqqisoutofpower <2651209638@qq.com>
2022-11-19 16:28:19 +0800

    input

--- src/components/input.vue
+++ src/components/input.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="app">
-    <div>
+    <div class="largest">
+      <div class="leftbox">输入</div>
+      <div class="rightbox"></div>
     </div>
-    </ul>
-  </div>
 </template>
 
 <script lang="ts">
 
 export default {
-  name: 'app',
+  name: 'input',
   props: {
     ncIntPut: String,
     ncOutPut: String
@@ -19,21 +18,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+ .largest{
+  width:840px;
+  height:140px;
+ }
+ .leftbox{
+float:left;
+width: 281px;
+height: 140px;
+line-height: 52px;
+border-radius: 10px 10px 10px 10px;
+background-color: rgba(255, 255, 255, 1);
+color: rgba(16, 16, 16, 1);
+font-size: 36px;
+text-align: center;
+font-family: Roboto;
+border: 5px solid rgba(16, 16, 16, 1);
+ }
+ .rightbox{
+ float:right;
+width: 539px;
+height: 140px;
+border-radius: 15px 15px 15px 15px;
+color: rgba(16, 16, 16, 1);
+font-size: 48px;
+text-align: left;
+font-family: Roboto;
+border: 5px solid rgba(16, 16, 16, 1);
+ }
 </style>
