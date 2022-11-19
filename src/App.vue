a658cb133d571808eed5734166bc87850d7c833e
q1ly <3353877295@qq.com>
2022-11-19 15:29:40 +0800

    '背景'

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    
   </div>
 </template>
-
+import  "babel-polyfill"
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
