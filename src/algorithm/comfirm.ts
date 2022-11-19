002d3f0aa02e23c47f860cda22ecf5444d904bcc
XZhang785 <752646336@qq.com>
2022-11-19 22:31:03 +0800

    feat: 添加了检查输入和输入错误提示的接口

---
+++ src/algorithm/comfirm.ts
@@ -0,0 +1,61 @@
+export const judge = (tokens: string): string => {
+    /**
+     * 判断输入是否合法
+     * @param tokens: 输入的字符串
+     * @return 提示语句，如果输入正确会返回输入正确，如果输入错误会返回错误的原因
+     */
+
+    // 判断是否输入非法字符
+    let tokenRe: string = "[^0-9零壹贰叁肆伍陆柒捌玖拾佰仟万亿元整]"
+    if (tokens.search(tokenRe)) {
+        return "输入的字符不正确"
+    }
+
+    // 判断是否全为大写或全为小写且长度合法
+    let lowRe: string = "\d+"
+    let upRe: string = "[零壹贰叁肆伍陆柒捌玖拾佰仟万亿元整]"
+    if (tokens.search(lowRe)) {
+        if (tokens.length > 12){
+            return "输入的小写数字长度大于12"
+        }
+        if (tokens[0] == "0") {
+            return "小写数字的首位为0"
+        }
+    }
+    else if (tokens.search(upRe)) {
+        if (tokens.length > 25) {
+            return "输入的大写数字长度大于25"
+        }
+        // 判断大写数字语法逻辑是否正确
+        let validInfo: string = isValidUpNum(tokens)
+        if (validInfo != "合法"){
+            return validInfo
+        }
+    }
+    else {
+        return "混合输入大写和小写数字"
+    }
+    return "输入正确"
+}
+
+const isValidUpNum = (upNum: string): string => {
+    /**
+     * 判断大写数字是否合法
+     * @prama upNum : 大写数字字符串
+     * @return 提示语句，如果合法则返回合法，如果不合法则返回不合法的原因
+     */
+    if (! upNum.endsWith("元整")) {
+        return "没有“元整”结尾"
+    }
+    let  units: string[] = ["亿","万","仟","佰","拾"] 
+    for (let i:number = 0; i < units.length - 1; i++) {
+        for (let j:number = i+1; j < units.length; j++) {
+            let invalidToken: string = units[i] + units[j]
+            if (upNum.search(invalidToken)) {
+                return invalidToken+"不合语法"
+            } 
+        }
+    }
+    
+    return "合法"
+}
